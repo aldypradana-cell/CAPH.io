9c5edb72fcce2963016481cd535a78c436aa1e19
Aldy <aldypradana11111@gmail.com>
2026-02-15 07:41:27 +0800

    versi laragon

--- fintrack-laravel/vite.config.js
+++ fintrack-laravel/vite.config.js
@@ -5,10 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.tsx',
+            input: 'resources/js/app.tsx', // atau app.jsx jika Anda tidak pakai TypeScript
             refresh: true,
         }),
         react(),
     ],
+    optimizeDeps: {
+        include: ['react-is', 'recharts', 'prop-types'],
+    },
 });
